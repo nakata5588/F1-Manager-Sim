eeaa1a0e5d1649c208b15bcc380e888116b35dcd
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-10 00:16:07 +0100

    feat: expose simulation foundation API

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { createSeasonSnapshot } from "./domain/seasonSnapshot.js";
+export { createSaveWorld } from "./save/createSaveWorld.js";
+export { createRng } from "./sim/random.js";
+export { advanceDay } from "./sim/clock.js";
