801ecb364ae5dd4f2ba54238ca6c192afa5bd702
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-10 12:40:08 +0100

    feat: export lifecycle simulation APIs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { createSaveWorld } from "./save/createSaveWorld.js";
 export { deserializeSaveWorld, SAVE_FORMAT, SAVE_SCHEMA_VERSION, serializeSaveWorld } from "./save/serialization.js";
 export { createRng } from "./sim/random.js";
 export { advanceDay } from "./sim/clock.js";
-export { advanceDays, dispatchSimulationEvents, SIM_EVENT } from "./sim/timeEngine.js";
+export { advanceDays, dispatchSimulationEvents, initializeSimulation, SIM_EVENT } from "./sim/timeEngine.js";
 export { runHeadlessSimulation } from "./sim/headless.js";
 export { CONTRACT_EVENT, createContractMilestoneSystem } from "./sim/systems/contractMilestones.js";
+export { ENTITY_EVENT, createEntityAvailabilitySystem } from "./sim/systems/entityAvailability.js";
