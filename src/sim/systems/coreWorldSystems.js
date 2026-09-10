1799ab20afb8a03b307be6b2686cd777223739fa
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-10 13:18:31 +0100

    feat(sim): compose career development and retirement systems

--- src/sim/systems/coreWorldSystems.js
+++ src/sim/systems/coreWorldSystems.js
@@ -1,14 +1,20 @@
 import { createContractMilestoneSystem } from "./contractMilestones.js";
 import { createEntityAvailabilitySystem } from "./entityAvailability.js";
 import { createCareerLifecycleSystem } from "./careerLifecycle.js";
+import { createCareerDevelopmentSystem } from "./careerDevelopment.js";
+import { createRetirementSystem } from "./retirement.js";
+import { createRetirementEmploymentSystem } from "./retirementEmployment.js";
 import { createAiEmploymentDecisionSystem, createEmploymentMarketSystem } from "./employmentMarket.js";
 
 export function createCoreWorldSystems(options = {}) {
   return [
     createEntityAvailabilitySystem(),
     createCareerLifecycleSystem(),
+    createCareerDevelopmentSystem(),
+    createRetirementSystem(),
     createContractMilestoneSystem(),
     createEmploymentMarketSystem(),
+    createRetirementEmploymentSystem(),
     createAiEmploymentDecisionSystem({
       controlledTeamIds: options.controlledTeamIds ?? [],
       defaultContractYears: options.defaultContractYears ?? 2,
