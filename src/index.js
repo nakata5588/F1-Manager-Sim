a4b70d9c673ab6ef5f9aaa1698536accd69202ba
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-11 22:22:06 +0100

    Phase 32: export management core APIs

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ export { applySeasonPackOverlay, overlaySourceChecksum, SeasonPackOverlayError }
 export { loadSeasonPackRuntimePayload, validateSeasonPackRuntimePayload } from "./data/seasonPackRuntime.js";
 export { createSaveWorld } from "./save/createSaveWorld.js";
 export { deserializeSaveWorld, SAVE_FORMAT, SAVE_SCHEMA_VERSION, serializeSaveWorld } from "./save/serialization.js";
+export { addManagementInboxItem, archiveManagementInboxItem, ensureManagementInbox, listManagementInbox, managementInboxSummary, markManagementInboxRead, resolveManagementInboxDecision } from "./game/management/inbox.js";
+export { buildDriverScoutingReport, completeDriverScoutingAssignment, driverScoutingKnowledge, ensureScoutingState, listRecruitmentCandidates, SCOUTING_EVENT, scoutingSummary, setDriverShortlist, startDriverScoutingAssignment } from "./game/management/scouting.js";
+export { acceptDriverContractCounterEvent, CONTRACT_NEGOTIATION_EVENT, contractNegotiationSummary, ensureContractNegotiationState, evaluateDriverContractOffer, listContractNegotiations, openDriverContractNegotiation, submitDriverContractOfferEvent, withdrawDriverContractNegotiationEvent } from "./game/management/contracts.js";
 export { createRng } from "./sim/random.js";
 export { availableTyreCompounds, createRaceStrategyPlan, evaluateRaceStrategy } from "./sim/raceStrategy.js";
 export { currentStrategyStint, reviseRaceStrategy, reviseStartingTyreCompound, selectLiveTyreCompound } from "./sim/liveStrategy.js";
@@ -33,6 +36,9 @@ export { createCareerDevelopmentSystem } from "./sim/systems/careerDevelopment.j
 export { createRetirementSystem, retirementProbability } from "./sim/systems/retirement.js";
 export { createRetirementEmploymentSystem } from "./sim/systems/retirementEmployment.js";
 export { EMPLOYMENT_EVENT, createAiEmploymentDecisionSystem, createEmploymentMarketSystem } from "./sim/systems/employmentMarket.js";
+export { createScoutingManagementSystem } from "./sim/systems/scoutingManagement.js";
+export { createContractNegotiationSystem } from "./sim/systems/contractNegotiation.js";
+export { createManagementInboxSystem } from "./sim/systems/managementInbox.js";
 export { RACE_ENTRY_EVENT, createRaceEntrySystem } from "./sim/systems/raceEntry.js";
 export { TEAM_FINANCE_EVENT, createTeamEconomySystem } from "./sim/systems/teamEconomy.js";
 export { TEAM_DEVELOPMENT_EVENT, createTeamDevelopmentSystem } from "./sim/systems/teamDevelopment.js";
