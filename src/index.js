0adf266f9a7d0eb707b8f6b529d4cb49cc6d6bf9
nakata5588 <ricardo.mendes45@gmail.com>
2026-09-11 21:23:54 +0100

    Phase 30: export developer playtest application API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { entityVisibilityState, isEntityF1EligibleInSeason, isEntityTalentVisibl
 export { getSeasonReadiness, listSupportedSeasons, loadHistoricalSeason } from "./domain/historicalWorld.js";
 export { createSeasonDatabasePayload, loadSeasonDatabasePayload, mergeSeasonBoundaryReferences, SEASON_DATABASE_FORMAT, SEASON_DATABASE_SCHEMA_VERSION, validateSeasonDatabasePayload } from "./data/seasonDatabase.js";
 export { createCareerFromSeasonDatabase, validateSeasonDatabaseAgainstGlobal } from "./data/careerBootstrap.js";
+export { DeveloperPlaytestSession, listDeveloperPlaytestTeams } from "./app/developerPlaytest.js";
 export { loadSeasonPackPayload, parseSeasonPackPayload, seasonPackSheetRows, SeasonPackValidationError, validateSeasonPackPayload } from "./data/seasonPackLoader.js";
 export { applySeasonPackOverlay, overlaySourceChecksum, SeasonPackOverlayError } from "./data/seasonPackOverlay.js";
 export { loadSeasonPackRuntimePayload, validateSeasonPackRuntimePayload } from "./data/seasonPackRuntime.js";
